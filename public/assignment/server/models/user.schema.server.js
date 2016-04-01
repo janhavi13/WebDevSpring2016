cee942eed0edab706c97ae644be4845ea5257c9a
Janhavi Suresh Mahajan <janhavi@ccs.neu.edu>
2016-04-01 15:42:33 -0400

    schema

--- public/assignment/server/models/user.schema.server.js
+++ public/assignment/server/models/user.schema.server.js
@@ -14,19 +14,3 @@ module.exports =function(){
 
 };
 
-
-module.exports = function (mongoose,db) {
-
-    var UserSchema = new mongoose.Schema({
-        userid:String,
-        username:String,
-        firstname:String,
-        lastname:String,
-        email:String,
-        password:String
-
-    });
-
-    //var UserModel = mongoose.model("UserModel" , UserSchema);
-    return UserSchema;
-};

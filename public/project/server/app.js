e84522add9a8f83200a202c3bac2ff447cd17c35
Janhavi Suresh Mahajan <janhavi@ccs.neu.edu>
2016-04-17 13:41:29 -0400

    reverting server code for song schema

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -1,8 +1,8 @@
 module.exports=function(app,uuid,db){
     var userModel=require("./model/user.model.js")(uuid,db);
-    var songModel=require("./model/song.model.js")(uuid,db);
+    //var songModel=require("./model/song.model.js")(uuid,db);
 
     var userService = require("./services/user.service.server.js")(app,userModel);
-    var eventService = require("./services/songs.service.server.js")(app,songModel);
+  //  var songService = require("./services/songs.service.server.js")(app,songModel);
 
 }

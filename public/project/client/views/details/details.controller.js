5c1ae2108fd936c8875a6374a76911948e0574d3
Janhavi Suresh Mahajan <janhavi@ccs.neu.edu>
2016-04-17 02:08:51 -0400

    removed fancy UI

--- public/project/client/views/details/details.controller.js
+++ public/project/client/views/details/details.controller.js
@@ -3,53 +3,27 @@
     angular.module("MusicApp")
         .controller("DetailsController",DetailsController);
 
-    function DetailsController($scope,$routeParams,$rootScope,SongService,  $location,$http){
+    function DetailsController($scope,$routeParams,$http){
             var trackID= $routeParams.trackID;
-        var vm = this;
-        var songID = $routeParams.trackID;
-        var currentUser = $rootScope.currentUser;
-       vm.favorite = favorite;
 
         $http.get("https://api.spotify.com/v1/tracks/"+trackID)
             .success(renderDetails);
 
-        function init() {
-
-           /* SpotifyService
-                .findSongBytrackID (trackID)
-                .then(function(response){
-                    vm.data = response.data;
-                });
-
-            SongService
-                .findUserLikes (songID)
-                .then(function(response){
-                    vm.song = response.data;
-                });*/
-        }
-
-
-
-
-        init();
-
         function renderDetails(response){
-            console.log("&&&");
             $scope.details=response;
         }
 
+        function likeSong(name){
 
-            function favorite(songID) {
-                if(currentUser) {
-                    vm.song.likes = [];
-                    vm.song.likes.push(currentUser._id);
-                    SongService
-                        .userLikesSong(currentUser._id, songID);
-                } else {
-                    $location.url("/login");
-                }
+            if($rootScope.user){
+                //console.log(imdbID + poster + title);
+                UserService.addSongLike(name,$rootScope.user._id);
+
+            }else {
+
+                $location.url("/login");
             }
-
+        }
 
 
 
